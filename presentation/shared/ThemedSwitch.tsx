30006273d0a45fe12095bd9878287eaba0614bda
Ricardo Badillo <ricardo94badillo@gmail.com>
2025-08-22 00:33:57 -0400

    Solucionar error de typo

--- presentation/shared/ThemedSwitch.tsx
+++ presentation/shared/ThemedSwitch.tsx
@@ -1,35 +1,40 @@
-import { Platform, Pressable, Switch, View } from 'react-native';
+import { Platform, Pressable, Switch, View } from "react-native";
 
-import { useThemeColor } from '@/hooks/useThemeColor';
-import ThemedText from './ThemedText';
+import { useThemeColor } from "@/hooks/useThemeColor";
+import ThemedText from "./ThemedText";
 
 interface Props {
-    className?: string;
-    text?: string;
-    value: boolean;
-    onValueChange: (value: boolean) => void;
+  className?: string;
+  text?: string;
+  value: boolean;
+  onValueChange: (value: boolean) => void;
 }
 
-const isAndroid = Platform.OS === 'android';
+const isAndroid = Platform.OS === "android";
 
-export default function ThemedSwitch({ className, text, value, onValueChange }: Props) {
-  const switchAciveColor = useThemeColor({}, 'primary');
+export default function ThemedSwitch({
+  className,
+  text,
+  value,
+  onValueChange,
+}: Props) {
+  const switchActiveColor = useThemeColor({}, "primary");
 
   return (
     <Pressable
       className={`flex flex-row items-center justify-between mx-2 active:opacity-80 ${className}`}
-      onPress={ () => onValueChange(!value) }
+      onPress={() => onValueChange(!value)}
     >
-        { text ? <ThemedText type='h2'>{text}</ThemedText> : <View />}
-        <Switch
-            onValueChange={onValueChange}
-            thumbColor={isAndroid ? switchAciveColor : '' }
-            trackColor={{
-              false: 'grey',
-              true: switchAciveColor
-            }}
-            value={value}
-        />
+      {text ? <ThemedText type="h2">{text}</ThemedText> : <View />}
+      <Switch
+        onValueChange={onValueChange}
+        thumbColor={isAndroid ? switchActiveColor : ""}
+        trackColor={{
+          false: "grey",
+          true: switchActiveColor,
+        }}
+        value={value}
+      />
     </Pressable>
-  )
+  );
 }
